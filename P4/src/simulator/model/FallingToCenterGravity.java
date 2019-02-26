ba8e5426df5e8701093df359b9e251951b2ae0a9
diegoalr <diegoalr@ucm.es>
2019-02-26 17:02:39 +0100

    Update FallingToCenterGravity.java

--- P4/src/simulator/model/FallingToCenterGravity.java
+++ P4/src/simulator/model/FallingToCenterGravity.java
@@ -6,20 +6,57 @@ import simulator.misc.Vector;
 
 public class FallingToCenterGravity implements GravityLaws {
 
-	@Override
-	public void apply(List<Body> bodies) {
-		// TODO Auto-generated method stub
-
+	private Vector fuerza;
+	private double g; //gravedad
+	
+	public FallingToCenterGravity(){
+		
+		 this.fuerza = null;
+		 this.g = (-9.81);
 	}
 	
-	double g = 9.81;
+	@Override
+	public void apply(List<Body> bodies) {
+		
+		for (int i = 0; i < bodies.size(); i++)
+		{	
+			
+			FuerzaAplicadaSobreElObjeto(bodies.get(i));
+		
+		}
+	}
+	
+	//private double g = (-9.81); //gravedad
+	
+	public Vector FuerzaAplicadaSobreElObjeto(Body cuerpo){
+		//Vector fuerza1;
+		//Vector Origin = new Vector(0);
+		
+		fuerza = cuerpo.getAcceleration().scale(g);
+		AplicaAceleracion(cuerpo);
+		
+		//fuerza1 = cuerpo.getPosition().scale(g);
+		//fuerza1 = fuerza.distanceTo(Origin);
+		
+		return fuerza;
+	}
+	
+	public void AplicaAceleracion(Body cuerpo) {
+		
+		cuerpo.setAcceleration(fuerza);
+		
+	}
+	
+	/*
+	double g = -9.81;
 	Vector coordenadas;
 	
 	public Vector cayendo (double _data[], int i, Vector d1) {
 		
-		coordenadas = d1.scale(-g);
+		coordenadas = d1.scale(g);
 		
 		return coordenadas;
 		
 	}
+	*/
 }
